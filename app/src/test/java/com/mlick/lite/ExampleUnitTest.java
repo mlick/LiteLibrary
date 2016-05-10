3c2b0420933ab174b9b0447f98be63f233294392
mlick <2010lixiangxin@163.com>
2016-05-10 09:51:27 +0800

    初始化导入

---
+++ app/src/test/java/com/mlick/lite/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+package com.mlick.lite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+        System.out.print("out");
+    }
+
+    @Test
+    public void showMainMethod() {
+        System.out.print(MainActivity.getString());
+    }
+}
