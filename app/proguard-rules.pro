a94797106409d3ae59bf58619ddd8e71a2484d7d
mlick <2010lixiangxin@163.com>
2016-05-17 17:14:00 +0800

    添加butterknife的demo

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,31 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+-keepattributes EnclosingMethod
+
+-ignorewarnings                     # 忽略警告，避免打包时某些警告出现
+-optimizationpasses 5               # 指定代码的压缩级别
+-dontusemixedcaseclassnames         # 是否使用大小写混合
+-dontskipnonpubliclibraryclasses    # 是否混淆第三方jar
+-dontpreverify                      # 混淆时是否做预校验
+-verbose                            # 混淆时是否记录日志
+
+-optimizations !code/simplification/arithmetic,!field/*,!class/merging/*    # 混淆时所采用的算法
+
+-dontwarn android.support.v4.**
+-dontwarn android.os.**
+-keep class android.support.v4.** { *; }        # 保持哪些类不被混淆
+-keep class android.os.**{*;}
+
+-keep interface android.support.v4.app.** { *; }
+-keep public class * extends android.support.v4.**
+-keep interface android.support.v7.** { *; }
+-keep public class * extends android.support.v7.**
+
+-keep public class * extends android.app.Fragment
+-keep public class * extends android.app.Activity
+-keep public class * extends android.app.Application
+-keep public class * extends android.app.Service
+-keep public class * extends android.content.BroadcastReceiver
+-keep public class * extends android.content.ContentProvider
+-keep public class * extends android.support.v4.widget
--- app/src/main/java/com/mlick/lite/MainActivity.java
+++ app/src/main/java/com/mlick/lite/MainActivity.java
@@ -2,13 +2,30 @@ package com.mlick.lite;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_tv)
+    TextView helloTv;
+    @BindView(R.id.click_btn)
+    Button clickBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        helloTv.setText("sayHello");
+        clickBtn.setText("show Hello");
 //        Retrofit retrofit = new Retrofit.Builder()
 //                .baseUrl("https://api.github.com/")
 //                .build();
@@ -29,8 +46,19 @@ public class MainActivity extends AppCompatActivity {
 //        }
     }
 
-
     public static String getString() {
         return MainActivity.class.getName();
+    }
+
+    @OnClick({R.id.hello_tv, R.id.click_btn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.hello_tv:
+                Toast.makeText(this, "hello_tv", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.click_btn:
+                Toast.makeText(this, "click_btn", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
