dc41959228b914225c3a86ce3f9b986c7b984705
mlick <2010lixiangxin@163.com>
2016-05-20 09:43:52 +0800

    添加处理异常的工具类

---
+++ app/src/main/java/com/mlick/example/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.mlick.example;
+
+import android.app.Application;
+
+import com.lite.library.utils.CrashHandler;
+
+/**
+ * Created by lxx on 2016/5/20 9:32
+ */
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //注册crashHandler
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+}
